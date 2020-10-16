c545187bd5a445604a5bf8d1aca67942c92978d5
Krishna Kumar <ggkrish3093@gmail.com>
2020-10-16 20:53:32 +0530

    Export data column selecting fix - Grid

--- grid/src/Customgrid.js
+++ grid/src/Customgrid.js
@@ -645,7 +645,9 @@ const Customgrid = (props) => {
                                     toggleExportDataOverlay
                                 }
                                 rows={rows}
-                                columns={gridColumns}
+                                columns={JSON.parse(
+                                    JSON.stringify(gridColumns)
+                                )}
                                 additionalColumn={additionalColumn}
                             />
                         </div>
