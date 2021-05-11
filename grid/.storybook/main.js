91efa5335ea79f74f2a0d8034a3ace69ccf44edc
jayakrishnanrnair <63115526+jayakrishnanrnair@users.noreply.github.com>
2021-05-11 14:19:19 +0530

    Update: storybook config as given in the iCargo repo

--- grid/.storybook/main.js
+++ grid/.storybook/main.js
@@ -13,5 +13,60 @@ module.exports = {
         "@storybook/addon-knobs/register",
         "@panosvoudouris/addon-versions/register"
     ],
-    presets: ["@storybook/addon-docs/preset"]
+    presets: ["@storybook/addon-docs/preset"],
+    webpackFinal: async (config, { configType }) => {
+        // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+        // You can change the configuration based on that.
+        // 'PRODUCTION' is used when building the static version of storybook.
+
+        // Make whatever fine-grained changes you need
+        config.module.rules.push({
+            test: /\.(scss)$/,
+            exclude: /node_modules/,
+            use: [
+                {
+                    loader: "style-loader",
+                    options: {
+                        injectType: "lazySingletonStyleTag"
+                    }
+                },
+                {
+                    loader: "css-loader",
+                    options: {
+                        modules: false
+                    }
+                },
+                {
+                    loader: "sass-loader",
+                    options: {
+                        implementation: require("sass")
+                    }
+                }
+            ]
+        });
+        config.module.rules.push({
+            test: /\.(scss)$/,
+            include: /node_modules/,
+            use: [
+                {
+                    loader: "style-loader"
+                },
+                {
+                    loader: "css-loader",
+                    options: {
+                        modules: false
+                    }
+                },
+                {
+                    loader: "sass-loader",
+                    options: {
+                        implementation: require("sass")
+                    }
+                }
+            ]
+        });
+
+        // Return the altered config
+        return config;
+    }
 };
